45f87e76b6fb6ccf7a1b98a87d1d3d54f24123f5
xue <thecaichicken@gmail.com>
2025-03-14 16:59:51 +0800

    2025.3.14

--- source/isaaclab_tasks/isaaclab_tasks/direct/ergonomic_hrta/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/ergonomic_hrta/__init__.py
@@ -17,11 +17,11 @@ from . import agents
 
 gym.register(
     order_enforce=False,
-    id="Isaac-TaskAllocation-Direct-v0",
-    entry_point=f"{__name__}.hrta_env:HRTaskAllocEnv",
+    id="Isaac-TaskAllocation-Direct-v1",
+    entry_point=f"{__name__}.eg_hrta_env:HRTaskAllocEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.hrta_env_cfg:HRTaskAllocEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.eg_hrta_env_cfg:HRTaskAllocEnvCfg",
         "rainbowmini": f"{agents.__name__}:rainbowmini.yaml",
         # "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:CartpolePPORunnerCfg",
         # "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
