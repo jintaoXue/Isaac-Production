408656497e24888f8aa80ea2e9bfb03ed850c05f
xue <thecaichicken@gmail.com>
2025-07-27 11:39:20 +0800

    train sytep 3e6

--- source/algo/safe_rl/dqn.py
+++ source/algo/safe_rl/dqn.py
@@ -42,7 +42,7 @@ class DqnAgent():
         # self.update_frequency_sfl = config.get('update_frequency', 100)
         self.evaluate_interval = config.get('evaluate_interval', 400)
         self.target_update = config.get('target_update', int(2e3))
-        self.max_steps = config.get("max_steps", int(2e6))
+        self.max_steps = config.get("max_steps", int(3e6))
         self.max_epochs = config.get("max_epochs", int(1e11))
         self.batch_size = config.get('batch_size', 512)
         self.num_warmup_steps = config.get('num_warmup_steps', int(5e4))
