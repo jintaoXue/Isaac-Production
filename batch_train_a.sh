4b133c68787983d8feb1e5f826651b311541330a
xue <thecaichicken@gmail.com>
2025-07-28 18:27:49 +0800

    change batch training

--- batch_train_a.sh
+++ batch_train_a.sh
@@ -1,10 +1,8 @@
-
-
 python train.py --task Isaac-TaskAllocation-Direct-v1 --algo rl_filter --headless --wandb_activate
 
-python train.py --task Isaac-TaskAllocation-Direct-v1 --algo dqn --headless --wandb_activate --use_fatigue_mask
-
 python train.py --task Isaac-TaskAllocation-Direct-v1 --algo ppo_dis --headless --wandb_activate
+
+python train.py --task Isaac-TaskAllocation-Direct-v1 --algo dqn --headless --wandb_activate --use_fatigue_mask
 
 python train.py --task Isaac-TaskAllocation-Direct-v1 --algo ppo_dis --headless --wandb_activate --use_fatigue_mask
 
