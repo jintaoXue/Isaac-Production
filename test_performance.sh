6f60affad57ad3c16a4fe0b09e53d9309c4c92e7
XUE_Jintao <thecaichicken@gmail.com>
2025-08-03 17:30:15 +0800

    change test setting

--- test_performance.sh
+++ test_performance.sh
@@ -1,32 +1,55 @@
-#！/bin/bash
-load_dir="/rl_filter_2025-07-20_12-17-12/nn"
-relative_pth="/logs/rl_games/HRTA_direct"
-str="/"
-work_space_path=$(pwd)
-dir_path=$work_space_path$relative_pth$load_dir
-# path=$1
-files=$(ls $dir_path)
+# #！/bin/bash
+# load_dir="/rl_filter_2025-07-20_12-17-12/nn"
+# relative_pth="/logs/rl_games/HRTA_direct"
+# str="/"
+# work_space_path=$(pwd)
+# dir_path=$work_space_path$relative_pth$load_dir
+# # path=$1
+# files=$(ls $dir_path)
 
+
+
+############### Part 1 in workstation 4090 ###############
 list=(
-    1
-    # 2
-    # 3
-    # 4
-    # 5
+ 23700
+ 22900
+ 24800
+ 18100
 )
 
+for num in $list
+do
+    python train.py --task Isaac-TaskAllocation-Direct-v1 --algo dqn --headless --wandb_activate --test \
+        --load_dir "/dqn_2025-07-29_13-21-06/nn" --load_name "/HRTA_direct_ep_$num.pth" --wandb_project test_HRTA_fatigue --test_times 50
+done
 
-python train.py --task Isaac-TaskAllocation-Direct-v1 --algo dqn --headless --wandb_activate --test --use_fatigue_mask \
-    --load_dir "/dqn_2025-07-29_13-21-06/nn" --load_name "/HRTA_direct_ep_84400.pth" --wandb_project test_HRTA_fatigue --test_times 1
+# 1. D3QN nomask_4090_rl_filter_2025-07-25_15-02-16
+list=(
+ 23700
+ 22900
+ 24800
+ 18100
+)
+
+for num in $list
+do
+    python train.py --task Isaac-TaskAllocation-Direct-v1 --algo dqn --headless --wandb_activate --test \
+        --load_dir "/dqn_2025-07-29_13-21-06/nn" --load_name "/HRTA_direct_ep_$num.pth" --wandb_project test_HRTA_fatigue --test_times 50
+done
 
 
 
 
-# for num in $list
-# do
-#     python train.py --task Isaac-TaskAllocation-Direct-v1 --algo rl_filter --headless --wandb_activate --test \
-#         --load_dir "$load_dir" --load_name "/HRTA_direct_ep_82400.pth" --wandb_project test_HRTA_fatigue --test_times 10
-# #    echo $filename >> filename.txt
-# #    echo -e >> filename.txt
-# done
+############### Part 2 in workstation 4070 ###############
+list=(
+ 23700
+ 22900
+ 24800
+ 18100
+)
 
+for num in $list
+do
+    python train.py --task Isaac-TaskAllocation-Direct-v1 --algo dqn --headless --wandb_activate --test \
+        --load_dir "/dqn_2025-07-29_13-21-06/nn" --load_name "/HRTA_direct_ep_$num.pth" --wandb_project test_HRTA_fatigue --test_times 50
+done
