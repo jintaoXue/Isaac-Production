16e2e809f216e68d917c738d22931d499fdf92ed
xue <thecaichicken@gmail.com>
2025-11-28 16:53:52 +0800

    no dueling refine

--- source/algo/safe_rl/model_ablation.py
+++ source/algo/safe_rl/model_ablation.py
@@ -29,6 +29,10 @@ class MLPBlock(nn.Module):
         nn.ReLU(),
         nn.Linear(hidden_size, hidden_size),
         nn.ReLU(),
+        nn.Linear(hidden_size, hidden_size),
+        nn.ReLU(),
+        nn.Linear(hidden_size, hidden_size),
+        nn.ReLU(),
     )
     
   def forward(self, x):
